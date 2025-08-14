ad68134a2c157de5899b74d37041da1173c87d1f
GrassEater2 <tiantirta@gmail.com>
2025-08-14 08:49:27 +0700

    resetting project

--- Backend/routes/TransactionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createTransaction } = require('../controller/TransactionController.js');
-
-const router = express.Router();
-
-// Endpoint API
-router.post('/', createTransaction);
-
-module.exports = router;
